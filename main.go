04c5dd12b7501e46bd3f0e89c5bb7a4fcbab0514
carraes <carraeshb@gmail.com>
2023-04-14 10:36:11 -0300

    feat: consumer/producer problem

--- main.go
+++ main.go
@@ -1,4 +1,120 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order for pizza #%d\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d, will take %d seconds\n", pizzaNumber, delay)
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("Pizza #%d is overcooked", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("Pizza #%d is undercooked", pizzaNumber)
+		} else {
+			msg = fmt.Sprintf("Pizza #%d is perfect", pizzaNumber)
+			success = true
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	i := 0
+
+	for {
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <-pizzaMaker.quit:
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
+}
+
 func main() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	color.Cyan("The Pizzeria is open for business!")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
+
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				color.Green(i.message)
+			} else {
+				color.Red(i.message)
+			}
+		} else {
+			color.Cyan("The Pizzeria is closed for business!")
+			err := pizzaJob.Close()
+			if err != nil {
+				color.Red(err.Error())
+			}
+		}
+	}
 }
